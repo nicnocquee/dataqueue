20c7438aedfb1fac26148d5cfb07442201490139
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2025-06-29 23:56:42 +0200

    update docs

--- apps/docs/source.config.ts
+++ apps/docs/source.config.ts
@@ -4,6 +4,8 @@ import {
   frontmatterSchema,
   metaSchema,
 } from 'fumadocs-mdx/config';
+import { remarkInstall } from 'fumadocs-docgen';
+import { rehypeCode, remarkAdmonition } from 'fumadocs-core/mdx-plugins';
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
@@ -18,6 +20,6 @@ export const docs = defineDocs({
 
 export default defineConfig({
   mdxOptions: {
-    // MDX options
+    remarkPlugins: [remarkInstall, remarkAdmonition],
   },
 });
